c791b83f78c820b15622cdae6b9331af640170e0
mineot <mineot@gmail.com>
2026-07-10 10:39:14 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <Header />
+  <main class="app-main">
+    <div>Inicio</div>
+    <div v-for="test in tests" :key="test">Content</div>
+    <div>Final</div>
+  </main>
+</template>
+
+<script setup lang="ts">
+import Header from './components/header.vue';
+const tests = Array.from({ length: 50 }, (_, i) => i);
+</script>
